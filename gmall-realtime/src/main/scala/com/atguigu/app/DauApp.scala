249e77ac2cebd8b5335df8d4a2dca5eb2619f25c
ZhangJY0426 <atguigu0426@gg.com>
2021-09-27 15:37:08 +0800

    day01-上午

---
+++ gmall-realtime/src/main/scala/com/atguigu/app/DauApp.scala
@@ -0,0 +1,37 @@
+package com.atguigu.app
+
+import com.atguigu.constants.GmallConstants
+import com.atguigu.utils.MyKafkaUtil
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.dstream.InputDStream
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+object DauApp {
+  def main(args: Array[String]): Unit = {
+    //1.创建SparkConf
+    val sparkConf: SparkConf = new SparkConf().setAppName("DauApp").setMaster("local[*]")
+
+    //2.创建StreamingContext
+    val ssc: StreamingContext = new StreamingContext(sparkConf,Seconds(3))
+
+    //3.消费kafka数据
+    val kafkaDStream: InputDStream[ConsumerRecord[String, String]] = MyKafkaUtil.getKafkaStream(GmallConstants.KAFKA_TOPIC_STARTUP,ssc)
+
+    //4.打印kafka中的数据
+    kafkaDStream.foreachRDD(rdd=>{
+      rdd.foreachPartition(patition=>{
+        patition.foreach(record=>{
+          println(record.value())
+        })
+      })
+    })
+
+
+    //开启任务
+    ssc.start()
+    //阻塞任务
+    ssc.awaitTermination()
+  }
+
+}
